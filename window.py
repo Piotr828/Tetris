55b074d1e39c470323494bc0e6f55184ee08eb3f
Piotr828 <353819@uwr.edu.pl>
2024-12-12 16:27:09 +0100

    Dodano requirements.txt

--- window.py
+++ window.py
@@ -1,12 +1,16 @@
 import webview
 
 
-with open('api.js', 'w') as file:
-    file.write("var pyvars = {}\n")
-webview.create_window('Projekt', "index.html")
-import main
-webview.start()
-import preend
-import end
-with open('api.js', 'w') as file:
-    file.write("")
+def main():
+    with open('api.js', 'w') as file:
+        file.write("var pyvars = {}\n")
+    webview.create_window('Tetris', "index.html")
+    import main
+    webview.start()
+    import preend
+    import end
+    with open('api.js', 'w') as file:
+        file.write("")
+# Punkt startowy
+if __name__ == "__main__":
+    main()
