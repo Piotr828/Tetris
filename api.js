f6a969938ade4e8b7f8fa8bcdf93d163ffe308f9
Piotr828 <353819@uwr.edu.pl>
2024-12-27 03:37:35 +0100

    Poprawiono animację w menu na siniku mozzilla

--- api.js
+++ api.js
@@ -1,1 +1,11 @@
 function exec_py(functionName, ...args) {return window.pywebview.api.call_function(functionName, args).then(response => {return response;});}
+let lastReloadTime = 0; // Zmienna przechowująca czas ostatniego wywołania funkcji
+function reloadCSS() {
+    const currentTime = new Date().getTime(); // Pobieramy bieżący czas
+    if (currentTime - lastReloadTime >= 8000) { // Sprawdzamy, czy minęło 8 sekund
+        document.getElementById('toreload').href = document.getElementById('toreload').href + "?v="+currentTime
+    }
+}
+window.addEventListener('resize', function() {
+    reloadCSS()
+});
--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-// tutaj kod JavaScipt
+function startgame(){
+document.addEventListener('keydown', function(event) {
+    // Sprawdzanie naciśnięcia strzałki w prawo lub 'd'
+    if (event.key === 'ArrowRight' || event.key === 'd') {
+        console.log('Strzałka w prawo lub d');
+                document.getElementById("main").innerText += ">"
+
+    }
+
+    // Sprawdzanie naciśnięcia strzałki w lewo lub 'a'
+    else if (event.key === 'ArrowLeft' || event.key === 'a') {
+        console.log('Strzałka w lewo lub a');
+       document.getElementById("main").innerText += "<"
+    }
+
+    // Sprawdzanie naciśnięcia strzałki w dół, 's' lub spacji
+    else if (event.key === 'ArrowDown' || event.key === 's' || event.key === ' ') {
+        console.log('Strzałka w dół, s lub spacja');
+        document.getElementById("main").innerText += "v"
+        // Dodaj odpowiednią akcję tutaj
+    }
+});}
