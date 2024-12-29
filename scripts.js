52d369254c5f5afaa256170956877668824fa595
HannaWaronska <waronskahanna22@gmail.com>
2024-12-29 12:33:13 +0100

    zmiana funkcjii usuwającej wiersze

--- scripts.js
+++ scripts.js
@@ -42,33 +42,39 @@ const klocki = {
         [1, 0, 0, 0],
     ],
 };
-const kolory = ['blue','green','orange','purple','yellow','red','blue2']
-kolor = kolory[Math.floor(Math.random()*kolory.length)]
+
+const matrix = document.getElementById("main_js");
+const kolory = ['blue','green','orange','purple','yellow','red','blue2'];
+kolor = kolory[Math.floor(Math.random()*kolory.length)];
 let row = 0; // wiersz klocka
  // kolumna klocka
-let klocek = klocki['klocek'+Math.ceil(Math.random()*7)]
+let klocek = klocki['klocek'+Math.ceil(Math.random()*7)];
 const rows = 20;
 const columns = 10;
 let plansza = Array.from({ length: rows }, () => Array(columns).fill(0));
 
-let column = Math.floor(Math.floor(Math.random()*(10-szerokosc(klocek))) )
+let column = Math.floor(Math.floor(Math.random()*(10-szerokosc(klocek))) );
 
-function dodajKlocekNaPlansze(plansza, klocek, startRow, startCol) { // dodanie klocka na plansze, gdzie zaczyna sie jego pozycja na startRow i startCol
-    for (let i = 0; i < klocek.length; i++) {
-        for (let j = 0; j < klocek[i].length; j++) {
-            if (klocek[i][j] === 1) {
-                const row = startRow + i;
-                const col = startCol + j;
-                if (row >= 0 && row < plansza.length && col >= 0 && col < plansza[0].length) {
-                    plansza[row][col] = 1;
-                }
-            }
-        }
+// function dodajKlocekNaPlansze(plansza, klocek, startRow, startCol) { // dodanie klocka na plansze, gdzie zaczyna sie jego pozycja na startRow i startCol
+//     for (let i = 0; i < klocek.length; i++) {
+//         for (let j = 0; j < klocek[i].length; j++) {
+//             if (klocek[i][j] === 1) {
+//                 const row = startRow + i;
+//                 const col = startCol + j;
+//                 if (row >= 0 && row < plansza.length && col >= 0 && col < plansza[0].length) {
+//                     plansza[row][col] = 1;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+function usunPelneWiersze(plansza) { // usuwa wiersze gdzie sa same 1 i dodaje z 0 na górze
+    const noweWiersze = plansza.filter(row => row.includes(0));
+    while (noweWiersze.length < plansza.length) {
+        noweWiersze.unshift(Array(plansza[0].length).fill(0));
     }
-}
-
-function usunPelneWiersze(plansza) { // usuwa wiersze gdzie sa same 1
-    return plansza.filter(row => row.includes(0));
+    return noweWiersze;
 }
 
 function szerokosc(T) {
@@ -101,10 +107,10 @@ function startgame(){
             if(column < 10 - szerokosc(klocek)){
                 column += 1;
             }
- let nr = parseInt(Object.values(klocki).indexOf(klocek))
+    let nr = parseInt(Object.values(klocki).indexOf(klocek))
                 nr++
                 let margin = 5*parseInt(column)-15
-            document.getElementById("main").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';        }
+            document.getElementById("main_js").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';        }
 
         // Sprawdzanie naciśnięcia strzałki w lewo lub 'a'
         else if (event.key === 'ArrowLeft' || event.key === 'a') {
@@ -112,10 +118,10 @@ function startgame(){
             if(column > 0){
                 column -= 1;
             }
- let nr = parseInt(Object.values(klocki).indexOf(klocek))
+    let nr = parseInt(Object.values(klocki).indexOf(klocek))
                 nr++
                 let margin = 5*parseInt(column)-15
-            document.getElementById("main").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';        }
+            document.getElementById("main_js").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';        }
 
         // Sprawdzanie naciśnięcia strzałki w dół, 's' lub spacji
         else if (event.key === 'ArrowDown' || event.key === 's' || event.key === ' ') {
@@ -124,7 +130,7 @@ function startgame(){
                 let nr = parseInt(Object.values(klocki).indexOf(klocek))
                 nr++
                 let margin = 5*parseInt(column)-15
-            document.getElementById("main").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';
+            document.getElementById("main_js").innerHTML = '<img style="bottom: 0vh; position: relative; left:'+margin+'vh" class="klocek_menu" src="images/Tetr'+nr+ '_' +kolor+ '.png">';
             }
         }
     });
