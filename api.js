cd5df706d9c1df8e11389afa92a85e2232901939
Piotr828 <353819@uwr.edu.pl>
2025-01-05 13:53:55 +0100

    Uniemożliwiono grę w pionowym oknie

--- api.js
+++ api.js
@@ -1,18 +1,37 @@
 function exec_py(functionName, ...args) {return window.pywebview.api.call_function(functionName, args).then(response => {return response;});}
 let lastReloadTime = 0; // Zmienna przechowująca czas ostatniego wywołania funkcji
 function reloadCSS() {
-    const currentTime = new Date().getTime(); // Pobieramy bieżący czas
-    if (currentTime - lastReloadTime >= 8000) { // Sprawdzamy, czy minęło 8 sekund
+    if (document.getElementById('toreload')) { // Sprawdzamy, czy minęło 8 sekund
         document.getElementById('toreload').href = document.getElementById('toreload').href + "?v="+currentTime
     }
 }
+window.addEventListener('resize', () => {
+  const html = document.documentElement;
+
+  // Pobranie aktualnej szerokości i wysokości okna
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  // Sprawdzenie warunku: wysokość > 2 * szerokość
+  if (height > 2 * width) {
+    html.style.filter = 'blur(50px)';
+    document.getElementById('main_js').style.filter = 'blor(50px)';
+    document.getElementById('main_js').style.display = 'none';
+
+  } else {
+    html.style.filter = 'none'; // Usunięcie efektu blur
+          document.getElementById('main_js').style.display = 'block';
+
+  }
+});
+
+// Wywołanie event listenera na start (aby sprawdzić aktualny stan okna)
+window.dispatchEvent(new Event('resize'));
 
 
 window.addEventListener('resize', function() {
     if(navigator.userAgent[0] == 'M'){
     reloadCSS()
         }
-    if(window.innerWidth < window.innerHeight) console.error("Granie w trybie pionowym jest niezalecane")
 });
 window.addEventListener('keydown', function(event) {
   if (event.key === 'Backspace') {
