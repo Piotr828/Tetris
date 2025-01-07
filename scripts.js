00a32ad3fd9bb7cead2617c1c23509e36d2937dc
laura <laura.olszewska0506@gmail.com>
2025-01-07 21:34:45 +0100

    Dodano zmianę w punktacji za ilość usuniętych wierszy.

--- scripts.js
+++ scripts.js
@@ -220,10 +220,19 @@ function dodajXP(XP){
 //Sztuczne logowanie
 saveSessionData('login','Piotr')
 function game_over(){
-    let punkty = Math.round(usuniete*10.4719755)
-exec_py('dodajXP', getSessionData('login'),punkty)
+    let punkty = 0;
+    if (usuniete == 1) {
+        punkty+=15;
+    } else if (usuniete == 2) {
+        punkty += 30;
+    } else if (usuniete ==3) {
+        punkty += 45;
+    } else if (usuniete == 4) {
+        punkty += 60;
+    } else (usuniete == 5) {
+        punkty += 75
+    }
 let x = `
-
 </head>
 <body>
     <div class="alert summary-alert">
